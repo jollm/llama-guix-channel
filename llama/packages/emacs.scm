6811a27ac1060f423d3987cd8c53fcb171a252ec
Fran Ley <fran.ley@mailfence.com>
2025-05-09 20:15:26 -0700

    Add package emacs-sync-recentf from melpa
    Allows syncing the recentf history between multiple Emacs instances

--- llama/packages/emacs.scm
+++ llama/packages/emacs.scm
@@ -502,3 +502,38 @@ is needed).")
      "Pollen mode provides editing assistant for pollen, the digital-publishing tool.
 See README for usage in detail.")
     (license #f)))
+
+(define-public emacs-sync-recentf
+  (package
+    (name "emacs-sync-recentf")
+    (version "20160326.2001")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/ffevotte/sync-recentf.git")
+             (commit "0052561d5c5b5c2684faedc3eead776aec06c3ed")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "006siydqxqds0qqds0zxn821dk4pw14wyymyp03n594wgqzw7m8q"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/ffevotte/sync-recentf")
+    (synopsis "Synchronize the recent files list between Emacs instances")
+    (description
+     "This package helps synchronizing the recent files list between Emacs instances.
+Without it, each Emacs instance manages its own recent files list.  The last one
+to close persistently saves its list into `recentf-save-file'; all files
+recently opened by other instances are overwritten.  With sync-recentf, all
+running Emacs instances periodically synchronize their local recent files list
+with `recentf-save-file'.  This ensures that all instances share the same list,
+which is persistently saved across sessions. `sync-recentf-marker is always
+pushed on top of `recentf-list when it is synchronized, after a load from
+`recentf-save-file or an explicit merge.  All files appearing before
+`sync-recentf-marker in `recentf-list were visited after the last
+synchronization, meaning that they should be pushed up in the next
+synchronization phase.  Synchronization actually happens at file save (see
+`recentf-save-list') or during periodical cleanups (see `recentf-cleanup') If
+you make improvements to this code or have suggestions, please do not hesitate
+to fork the repository or submit bug reports on github.  The repository is at:
+https://github.com/ffevotte/sync-recentf.")
+    (license #f)))
