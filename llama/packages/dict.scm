9bdf16d41395fa4e99c4c1a4073c02d0ef3d5c7c
Jo Gay <jo.gay@hpe.com>
2025-06-03 11:14:52 -0700

    Revert "Install dictionary files into individual subfolders"
    This reverts commit cc9b26b1b871c5a2d20f6745bea62fe4975244b5.

--- llama/packages/dict.scm
+++ llama/packages/dict.scm
@@ -19,32 +19,7 @@
 		"00raxdx6hl0dawhclzvvb1xy3j7g5j509l6ap3m3xpx5wz23585m"))))
     (build-system copy-build-system)
     (arguments
-     '(#:install-plan '(("." "share/dict/dictd/devils"
-                         #:include ("devils.dict" "devils.index"))
-                        ("." "share/dict/dictd/easton"
-                         #:include ("easton.dict" "easton.index"))
-                        ("." "share/dict/dictd/elements"
-                         #:include ("elements.dict" "elements.index"))
-                        ("." "share/dict/dictd/foldoc"
-                         #:include ("foldoc.dict" "foldoc.index"))
-                        ("." "share/dict/dictd/gazetteer"
-                         #:include ("gazetteer.dict" "gazetteer.index"))
-                        ("." "share/dict/dictd/gcide"
-                         #:include ("gcide.dict" "gcide.index"))
-                        ("." "share/dict/dictd/hitchcock"
-                         #:include ("hitchcock.dict" "hitchcock.index"))
-                        ("." "share/dict/dictd/jargon"
-                         #:include ("jargon.dict" "jargon.index"))
-                        ("." "share/dict/dictd/moby-thesaurus"
-                         #:include ("moby-thesaurus.dict" "moby-thesaurus.index"))
-                        ("." "share/dict/dictd/vera"
-                         #:include ("vera.dict" "vera.index"))
-                        ("." "share/dict/dictd/web1913"
-                         #:include ("web1913.dict" "web1913.index"))
-                        ("." "share/dict/dictd/wn"
-                         #:include ("wn.dict" "wn.index"))
-                        ("." "share/dict/dictd/world95"
-                         #:include ("world95.dict" "world95.index")))))
+     '(#:install-plan '(("." "share/dict/dictd"))))
     (home-page "https://github.com/ferdnyc/dictd-dicts")
     (synopsis "A collection of .dict and .index files for local dictionary servers")
     (description
